6ce2aa34ffd51e616fd750e6289dd6c329e8d440
carlsmith93 <cesmith@fullsail.edu>
2015-01-24 14:59:12 -0500

    added button and Picture JSON

--- Smith_Carl_Project3/Resources/app.js
+++ Smith_Carl_Project3/Resources/app.js
@@ -13,21 +13,8 @@ var Navigate = Ti.UI.iOS.createNavigationWindow({
 	window: Background
 });
 
-var StartButton = Ti.UI.createView({
-	backgroundColor: "gray",
-	left: 50,
-	right: 50,
-	height: 50
-});
 
-var ButtonLabel = Ti.UI.createLabel({
-	text: "Load Pictures",
-	font: {fontWeight: "Bold", fontFamily: "Verdana", fontSize: 15}
-	
-});
 
-StartButton.add(ButtonLabel);
-Background.add(StartButton);
 Navigate.open();
 
 

b1223de6c01160191fa5e235559f7cce3fb33ffc
carlsmith93 <cesmith@fullsail.edu>
2015-05-28 22:45:30 -0400

    Created application using Facebook API

---
+++ Smith_Carl_project1/build/android/gen/Smith/Carl/Project1/Smith_carl_project1Activity.java
@@ -0,0 +1,7 @@
+package Smith.Carl.Project1;
+
+import org.appcelerator.titanium.TiRootActivity;
+
+public final class Smith_carl_project1Activity extends TiRootActivity
+{
+}
---
+++ Smith_Carl_project1/build/android/gen/Smith/Carl/Project1/Smith_carl_project1AppInfo.java
@@ -0,0 +1,70 @@
+package Smith.Carl.Project1;
+
+import org.appcelerator.titanium.ITiAppInfo;
+import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.titanium.TiProperties;
+import org.appcelerator.kroll.common.Log;
+
+/* GENERATED CODE
+ * Warning - this class was generated from your application's tiapp.xml
+ * Any changes you make here will be overwritten
+ */
+public final class Smith_carl_project1AppInfo implements ITiAppInfo
+{
+	private static final String LCAT = "AppInfo";
+
+	public Smith_carl_project1AppInfo(TiApplication app) {
+	}
+
+	public String getDeployType() {
+		return "development";
+	}
+
+	public String getId() {
+		return "Smith.Carl.Project1";
+	}
+
+	public String getName() {
+		return "Smith_Carl_project1";
+	}
+
+	public String getVersion() {
+		return "1.0";
+	}
+
+	public String getPublisher() {
+		return "carlsmith";
+	}
+
+	public String getUrl() {
+		return "http://";
+	}
+
+	public String getCopyright() {
+		return "2015 by carlsmith";
+	}
+
+	public String getDescription() {
+		return "undefined";
+	}
+
+	public String getIcon() {
+		return "appicon.png";
+	}
+
+	public boolean isAnalyticsEnabled() {
+		return true;
+	}
+
+	public String getGUID() {
+		return "98090355-f8a4-4d34-8186-88dc58f85e29";
+	}
+
+	public boolean isFullscreen() {
+		return false;
+	}
+
+	public String getBuildType() {
+		return "";
+	}
+}
